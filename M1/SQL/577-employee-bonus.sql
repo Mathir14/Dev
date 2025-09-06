b89d44bbafa25dd0d0a6f789ccf8bef8ed08680e
Mathir14 <mathirvishnum2006@gmail.com>
2025-09-06 22:29:50 +0530

    Solved 577-emplpoyee-bonus problem

---
+++ M1/SQL/577-employee-bonus.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/employee-bonus/description/ --
+
+-- Write your PostgreSQL query statement below
+select Employee.name, Bonus.bonus
+from Employee
+left join Bonus
+on Employee.empid = Bonus.empid
+where Bonus.bonus < 1000 or Bonus.bonus is null;
