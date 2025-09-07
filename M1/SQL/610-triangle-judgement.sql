f7f59d2b7935065b04b67ab73ab2afb8ff432d3a
Mathir14 <mathirvishnum2006@gmail.com>
2025-09-07 22:39:53 +0530

    Solved 610-triangle-judgement problem

---
+++ M1/SQL/610-triangle-judgement.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/triangle-judgement/description/?envType=study-plan-v2&envId=top-sql-50 --
+
+select x,y,z,
+case when (x+y>z) and (x+z>y) and (y+z>x) then 'Yes' else 'No' end as triangle
+from Triangle;
