38e40bc199d3c1ab49487b573fbd0c6f1e62397b
Mathir14 <mathirvishnum2006@gmail.com>
2025-09-07 22:20:07 +0530

    Solved 1211-queries-quality-percentage problem

---
+++ M1/SQL/1211-queries-quality-percentage.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/queries-quality-and-percentage/description/?envType=study-plan-v2&envId=top-sql-50 --
+
+select query_name,
+round(avg(rating * 1.0 / position),2) as quality,
+round(sum(case when rating < 3 then 1.00 else 0.00 end) * 100.00 / count(*) , 2) as poor_query_percentage
+from Queries
+group by query_name;
